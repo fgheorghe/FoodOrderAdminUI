44a13cd4ba4598a1793fdddf533264e3ff5f71cd
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2014-12-06 18:33:39 +0000

    Partial API token UI logic.

--- app/source/api-access-token-settings.js
+++ app/source/api-access-token-settings.js
@@ -10,14 +10,63 @@ FOBO.ui.prototype.apiAccessTokenSettings = function() {
 };
 
 /**
+ * Makes a backend call for generating news access keys.
+ */
+FOBO.ui.prototype.apiAccessTokenSettings.prototype.generateNewKeys = function() {
+    // First, confirm with the user.
+    Ext.Msg.show({
+        title: 'Confirm key change?',
+        msg: 'Generating access keys requires update on API clients. Continue?',
+        buttons: Ext.Msg.YESNO,
+        icon: Ext.Msg.WARNING,
+        fn: function (btn) {
+            if (btn === 'yes') {
+                // TODO: Make API call.
+            }
+        }
+    });
+}
+
+/**
  * @function Initialises the object, by creating required panels and items.
  */
 FOBO.ui.prototype.apiAccessTokenSettings.prototype.init = function() {
+    // Form panel, hosting input fields.
+    this.form = Ext.create( 'Ext.form.Panel', {
+        defaultType: 'textfield',
+        frame: false,
+        border: false,
+        bodyPadding: 5,
+        items: [ {
+            fieldLabel: 'Access Token 1',
+            name: 'access_token_1',
+            labelAlign: 'right',
+            tabIndex: 1,
+            labelWidth: 150,
+            width: 350,
+            readOnly: true
+        }, {
+            fieldLabel: 'Access Token 2',
+            name: 'access_token_2',
+            editable: false,
+            labelAlign: 'right',
+            tabIndex: 2,
+            labelWidth: 150,
+            width: 350,
+            readOnly: true
+        } ]
+        ,buttons: [ {
+            text: 'Generate New Keys',
+            handler: function() {
+                this.generateNewKeys();
+            }.bind( this )
+        } ]
+    });
+
     // Panel itself.
     this.panel = Ext.create( 'Ext.panel.Panel', {
         title: "API Access Token Settings"
-        // TODO: Implement.
-        ,items: []
+        ,items: [this.form]
         ,layout: 'fit'
     } );
 }
